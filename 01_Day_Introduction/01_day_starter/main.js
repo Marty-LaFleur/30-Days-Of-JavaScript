c9cf9964d5dfdb34a3d9b6d2b720cdc02220a211
Marty-LaFleur <56937465+Marty-LaFleur@users.noreply.github.com>
2020-10-08 20:06:11 -0600

    Updated day 1 stuff
    doner than the donest done thing

--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,25 +1,20 @@
 // the variable values can be accessed from other variable.js files
-const personalInfo = {
-    firstName = marty,lastName = lafleur,country = USA,city = Muncie,age = 52,isMarried = false
+var personalInfo = {
+    firstName:"marty",lastName:"lafleur",country:"USA",city:"Muncie",age:52,isMarried:false
 }
-var personalInfo;
-    personalInfo = personalInfo;
+
   
 const geekyMathStuff = {
-    gravity = 9.81,boilingPoint = 100,PI = 3.14
+    gravity:9.81,boilingPoint:100,PI:3.14
 }
-var geekyMathStuff;
-    geekyMathStuff = geekyMathStuff;
 
-const morePersonalInfo = {
-    name = marty,
-    job = aWeSoMeOlOgIsT,
-    live = USA
+
+let morePersonalInfo = {
+    name:"marty",
+    job:"aWeSoMeOlOgIsT",
+    live:"USA"
     }
   
-var morePersonalInfo;
-  morePersonalInfo = morePersonalInfo;
-
 console.log (personalInfo);
 console.log (geekyMathStuff); 
 console.log (morePersonalInfo);
