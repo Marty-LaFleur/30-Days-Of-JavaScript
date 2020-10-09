323ba8847fafaad9ec690596310fedca2fcc961b
Marty-LaFleur <56937465+Marty-LaFleur@users.noreply.github.com>
2020-10-08 22:31:16 -0600

    Updating Day 2 stuff
    Day 2 is finished...

--- 02_Day_Data_types/string_data_types.js
+++ 02_Day_Data_types/string_data_types.js
@@ -1,7 +1,8 @@
-let space = ' ' // an empty space string
-let firstName = 'Asabeneh'
-let lastName = 'Yetayeh'
-let country = 'Finland'
-let city = 'Helsinki'
-let language = 'JavaScript'
-let job = 'teacher'
+let space = ' ';
+let firstName = 'Asabeneh';
+let lastName = 'Yetayeh';
+let age = 99;
+let country = 'Finland';
+let city = 'Helsinki';
+let language = 'JavaScript';
+let job = 'teacher';
