6704690090349ef20995fd6e83eccdc2430d2255
Marty-LaFleur <56937465+Marty-LaFleur@users.noreply.github.com>
2020-10-31 21:01:19 -0600

    Update martytest.js

--- 05_Day_Arrays/martytest.js
+++ 05_Day_Arrays/martytest.js
@@ -1,1 +1,6 @@
-mmm
+const companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'];
+ 
+console.log('Companies:', companies);
+ 
+var str = companies.join(' ')+'.';
+console.log(str);
