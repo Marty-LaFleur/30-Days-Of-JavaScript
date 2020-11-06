5d92a2c88375af22c81796633e87935255398a33
Marty-LaFleur <56937465+Marty-LaFleur@users.noreply.github.com>
2020-11-05 22:42:44 -0700

    Update martytest.js

--- 05_Day_Arrays/martytest.js
+++ 05_Day_Arrays/martytest.js
@@ -6,3 +6,25 @@ var str = companies.join(' ');
 
 console.log(companies.join(', '));
 console.log(str);
+
+console.log("Please enter a company name, and I will tell you if it is in the list");
+var company = window.prompt('Please enter a company.  Press OK when done.', '');
+console.log(company);
+ 
+if (company.match(/Facebook/gi)) {
+    console.log("Facebook is on the list")
+} else if (company.match(/Google/gi)) {
+    console.log("Google is on the list")
+} else if (company.match(/Microsoft/gi)) {
+    console.log("Google is on the list") 
+} else if (company.match(/Apple/gi)) {
+        console.log("Apple is on the list")
+} else if (userMonth.match(/IBM/gi)) {
+        console.log("IBM is on the list")
+} else if (userMonth.match(/Oracle/gi)) {
+            console.log("Oracle is on the list")
+} else if (userMonth.match(/Amazon/gi)) {
+            console.log("Amazon is on the list")
+} else if (userMonth.match(/August/gi)) {
+        console.log("Google is on the list") 
+} 
