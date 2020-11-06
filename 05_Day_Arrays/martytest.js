923655e2ddd650ca45cdb7e98cd3cd7e8d741d0f
Marty-LaFleur <56937465+Marty-LaFleur@users.noreply.github.com>
2020-11-06 00:23:55 -0700

    mmmm

--- 05_Day_Arrays/martytest.js
+++ 05_Day_Arrays/martytest.js
@@ -1,30 +1,13 @@
 const companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon'];
  
-//console.log('Companies:', companies);
- 
 var str = companies.join(' ');
 
-console.log(companies.join(', '));
-console.log(str);
-
-console.log("Please enter a company name, and I will tell you if it is in the list");
-var company = window.prompt('Please enter a company.  Press OK when done.', '');
-console.log(company);
+console.log('COMPANIES:',companies.join(', '));
  
-if (company.match(/Facebook/gi)) {
-    console.log("Facebook is on the list")
-} else if (company.match(/Google/gi)) {
-    console.log("Google is on the list")
-} else if (company.match(/Microsoft/gi)) {
-    console.log("Google is on the list") 
-} else if (company.match(/Apple/gi)) {
-        console.log("Apple is on the list")
-} else if (userMonth.match(/IBM/gi)) {
-        console.log("IBM is on the list")
-} else if (userMonth.match(/Oracle/gi)) {
-            console.log("Oracle is on the list")
-} else if (userMonth.match(/Amazon/gi)) {
-            console.log("Amazon is on the list")
-} else if (userMonth.match(/August/gi)) {
-        console.log("Google is on the list") 
-} 
+console.log("Facebook",companies[0].match(/o/gi))
+console.log("Google",companies[1].match(/o/gi))
+console.log("Microsoft",companies[2].match(/o/gi))
+console.log("Apple",companies[3].match(/o/gi))
+console.log("IBM",companies[4].match(/o/gi))
+console.log("Oracle",companies[5].match(/o/gi))
+console.log("Amazon",companies[6].match(/o/gi))
